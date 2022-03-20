9e3e5238afcc7a6f24a610d1c69cee5307cff8f6
Владимир Назаров <vova24848@gmail.com>
2022-03-20 08:40:59 +0300

    new run

--- __main__.py
+++ __main__.py
@@ -1,17 +1,25 @@
 import os
 import re
 import sys
-from subprocess import call
+from pathlib import Path
+from subprocess import call, run
 
-cmpl_msg = ""
-run_msg = ""
-
+# INIT
 try:
-    cmpl_msg = sys.argv[1]
-    run_msg = sys.argv[2]
+    cmpl_name = sys.argv[1]
+    program_name = sys.argv[2]
 except IndexError:
-    exit("you should run tester from console")
-    
+    exit("you should write options: compiler_name program_name")
+
+working_directory = Path(os.getcwd())
+testing_directory = Path(__file__).parent
+run_msg = testing_directory.joinpath("test")
+print(run_msg)
+run(f'{cmpl_name} -o {run_msg} {working_directory.joinpath(program_name)}')
+if not testing_directory.joinpath('test.exe').is_file():
+    exit('cant compile program')
+cases_directory = testing_directory.joinpath('tests')
+
 EPS = 0.001
 
 def read_to_array(n, file):
@@ -36,27 +44,27 @@ class Test:
         self.num = num
 
     def run(self, group_name):
-        result_filename = "test_output"
+        result_filename = testing_directory .joinpath("test_output")
         result = True
         try:
-            input_filename = f"tests/{group_name}/test{self.num}.in"
-            output_filename = f"tests/{group_name}/test{self.num}.out"
-            code_filename = f"tests/{group_name}/test{self.num}.code"
+            input_filename = f"{cases_directory}/{group_name}/test{self.num}.in"
+            output_filename = f"{cases_directory}/{group_name}/test{self.num}.out"
+            code_filename = f"{cases_directory}/{group_name}/test{self.num}.code"
 
             result_code = call(f"{run_msg} \"{input_filename}\" {result_filename}")
-            
+
             if result_code != 0:
                 try:
                     with open(code_filename) as code_file:
-                        code = int(code_file.read()) 
-                        return result_code == code       
+                        code = int(code_file.read())
+                        return result_code == code
                 except IOError:
                     return False
-                
+
             with open(input_filename, "r") as input_file, \
-                open(output_filename, "r") as output_file, \
-                open(result_filename, "r") as current_res:
-                
+                    open(output_filename, "r") as output_file, \
+                    open(result_filename, "r") as current_res:
+
                 n = int(input_file.readline())
                 s = [0] * (n + 1)
 
@@ -70,16 +78,15 @@ class Test:
             result = False
         finally:
             print(f"\tTest {self.num}:".ljust(10), "Passed" if result else "FAILED")
-            try: 
+            try:
                 os.remove(result_filename)
             finally:
                 return result
 
-
     def check(self, first, second, n):
         if type(first) != type(second):
             return False
-        
+
         if type(first) == type(second) == str:
             return first == second
 
@@ -88,20 +95,22 @@ class Test:
                 return False
         return True
 
+
 class Group:
     def __init__(self, name, frm, to):
         self.tests = []
         for i in range(frm, to + 1):
             self.tests.append(Test(i))
         self.name = name
-    
+
     def log(self):
         print(f"\n{self.name} Tests:\n")
+
 
 class Tester:
     def __init__(self, groups):
         self.groups = groups
-    
+
     def run(self):
         success = 0
         count = 0
@@ -131,10 +140,9 @@ def check_solution(n, coef, var):
             return False
     return True
 
-os.system(cmpl_msg)
 
 group1 = Group("Google Drive", 1, 13)
-group2 = Group("Very Small Number",  14, 53)
+group2 = Group("Very Small Number", 14, 53)
 group3 = Group("Small Number", 54, 68)
 group4 = Group("Moderate Number", 69, 80)
 group5 = Group("Moderate Number", 81, 100)
@@ -147,7 +155,7 @@ group11 = Group("Error Handling", 211, 214)
 group12 = Group("Float No solution", 213, 284)
 
 groups = [group1, group2, group3, group4,
-          group5, group6, group7, group8, 
+          group5, group6, group7, group8,
           group9, group10, group11, group12]
 
 tester = Tester(groups)
