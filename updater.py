e59a9294de75f59bbbf5fa6c0111e9b5654cd645
Владимир Назаров <vova24848@gmail.com>
2022-05-29 23:52:37 +0300

    1.3.1 fix updater

--- updater.py
+++ updater.py
@@ -4,14 +4,14 @@ main_branch = 'hw3'
 ex = 0
 print(f'SkakovLabO4ka {main_branch}')
 
-branch, _ = Popen(["git", "branch"], cwd='SkakovLabO4ka', stdout = PIPE).communicate()
+branch, _ = Popen(["git", "branch"], cwd='SkakovLabO4ka', stdout = PIPE, stderr = PIPE).communicate()
 for i in branch.decode().split('\n'):
     if main_branch not in i and '*' in i:
-        out, _ = Popen(["git", "checkout", main_branch], cwd = 'SkakovLabO4ka', stdout = PIPE).communicate()
+        out, _ = Popen(["git", "checkout", main_branch], cwd = 'SkakovLabO4ka', stdout = PIPE, stderr = PIPE).communicate()
         print(f'Change branch to {main_branch}.')
         ex = 1
 
-status, _ = Popen(["git", "pull"], cwd='SkakovLabO4ka', stdout = PIPE).communicate()
+status, _ = Popen(["git", "pull"], cwd='SkakovLabO4ka', stdout = PIPE, stderr = PIPE).communicate()
 if 'Already up to date.' not in status.decode():
     print('Update program.')
     ex = 1
