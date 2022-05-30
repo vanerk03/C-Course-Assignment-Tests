723b9aad60ade139173d90db80a52411489a6cac
vanerk03 <vanerk4@gmail.com>
2022-05-30 14:36:30 +0300

    1.4.1 generation implemented along with solve

--- generate.py
+++ generate.py
@@ -1,5 +1,46 @@
-"""This class is responsible for providing random data for test cases and solving"""
+"""
+This class is responsible for providing random data for test cases and solving
+
+cf grammar transitions
+1 -> 1 + 1
+1 -> 1 * 1
+1 -> 1 / 1
+1 -> 1 % 1
+
+1 -> 1 ~
+1 -> 1 _
+
+1 -> number
+"""
+
+from collections import namedtuple
 import random
+from math import sqrt
+
+"""
+#ToDo:
+
+"""
+Operation = namedtuple("Operation", ["arity", "oper"])
+
+table = {
+    "+": Operation(2, lambda x, y: x + y),
+    "-": Operation(2, lambda x, y: x - y),
+    "*": Operation(2, lambda x, y: x * y),
+    "/": Operation(2, lambda x, y: x // y),
+    "%": Operation(2, lambda x, y: x % y),
+
+    "~": Operation(1, lambda x: int(sqrt(x))),
+    "_": Operation(1, lambda x: -x),
+
+    # not clear
+    # "<" : None,
+    # "<=": None,
+    # ">" : None,
+    # ">=": None,
+    # "==": None,
+    # "!=": None,
+}
 
 
 def convert_data_to_string(data) -> str:
@@ -18,26 +59,50 @@ def convert_data_to_string(data) -> str:
     return out + '==\n'
 
 
-def generate_data(length: int):
-    """Generates list of given data type and size equal to length"""
-    lst = [generate_element(3, 3), "+", generate_element(3, 3)]
+def cf_grammar_transition(lst: list[int | str]) -> list[int | str]:
+    """Generates new expression from the given"""
+    new_lst = []
+    one_cnt = 1
+    for x in lst:
+        if x == 1 and random.random() < 1 / 2:
+            new_oper = random.choice(list(table.keys()))
+            arity = table[new_oper].arity
+            one_cnt += arity
+
+            if arity == 1:
+                new_lst.extend([1, new_oper])
+            else:
+                new_lst.extend([1, 1, new_oper])
+        else:
+            new_lst.append(x)
+    return new_lst
+
+
+def generate_data(n: int, min_digits: int = 5, max_digits: int = 5) -> list[int | str]:
+    """
+    Generates expression with sz elements, where sz is approximately n <= sz < n * 2,
+    and every numbers belongs lies within range [10**min_digits, 10**max_digits]
+    """
+    lst = [1]
+    while len(lst) < n:
+        lst = cf_grammar_transition(lst)
+    for i, x in enumerate(lst):
+        if x == 1:
+            lst[i] = generate_element(min_digits, max_digits)
     return lst
-
-
-"""
-#ToDo:
-create
-context-free grammar that allows only certain symbols:
-+, -, *, /, ~, %, <, <=, >, >=, ==, !=, _
-"""
 
 
 def generate_element(min_digits: int, max_digits: int) -> int:
     """Generates and returns an element in range [-10**mindigits, 10**max_digits]"""
-    return random.randint(-10 ** min_digits, 10 ** max_digits)
+    return random.randint(0, 10 ** max_digits)
 
 
 def answer(data: list):
     """data - list from function generated data"""
     # todo
     return
+
+
+# dbg
+if __name__ == "__main__":
+    pass
